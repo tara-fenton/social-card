baaf3fcc699a6ed2fe5517c64fbeedc02f2020f1
tara-fenton <tara@itsovertime.com>
2020-10-05 13:50:24 -0400

    2 cards

--- src/App.js
+++ src/App.js
@@ -6,36 +6,58 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      tweetData: {
-        avatar: "avatar-dev",
-        title: "Learning React? Start Small.",
-        topInfo: {
-          name: "The Practical Dev",
-          handle: "@ThePracticalDev",
-          date: "Sep 10",
-          author: "@dceddia",
+      tweetData: [
+        {
+          avatar: "avatar-dev",
+          title: "Learning React? Start Small.",
+          topInfo: {
+            name: "The Practical Dev",
+            handle: "@ThePracticalDev",
+            date: "Sep 10",
+            author: "@dceddia",
+          },
+          card: {
+            image: "social-card-image",
+            description:
+              "Can't pry yourself away from the tutorials? The cure is to make tiny little experiment apps.",
+            link: "dev.to",
+          },
+          socialBar: {
+            comments: 2,
+            retweets: 47,
+            likes: 190,
+          },
         },
-        card: {
-          image: "social-card-image",
-          description:
-            "Can't pry yourself away from the tutorials? The cure is to make tiny little experiment apps.",
-          link: "dev.to",
+        {
+          avatar: "avatar-dev",
+          title: "Learning React? Start Small.",
+          topInfo: {
+            name: "The Practical Dev",
+            handle: "@ThePracticalDev",
+            date: "Sep 10",
+            author: "@dceddia",
+          },
+          card: {
+            image: "social-card-image",
+            description:
+              "Can't pry yourself away from the tutorials? The cure is to make tiny little experiment apps.",
+            link: "dev.to",
+          },
+          socialBar: {
+            comments: 2,
+            retweets: 47,
+            likes: 190,
+          },
         },
-        socialBar: {
-          comments: 2,
-          retweets: 47,
-          likes: 190,
-        },
-      },
+      ],
     };
   }
 
   render() {
-    return (
-      <div className="App">
-        <SocialCard tweetData={this.state.tweetData} />
-      </div>
-    );
+    const tweets = this.state.tweetData.map((tweet) => {
+      return <SocialCard tweetData={tweet} />;
+    });
+    return <div className="app">{tweets}</div>;
   }
 }
 
